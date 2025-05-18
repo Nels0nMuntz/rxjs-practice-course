e49ca0957a67132d00e1ebc5c70fd7534d8adbf0
Eugene Chekerdes <kuharchuk.dmytro1337@gmail.com>
2025-05-18 22:38:38 +0300

    stream combination strategies

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,11 +1,11 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import moment from 'moment';
-import {fromEvent} from 'rxjs';
-import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap} from 'rxjs/operators';
-import {fromPromise} from 'rxjs/internal-compatibility';
+import { fromEvent } from 'rxjs';
+import { concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap, tap } from 'rxjs/operators';
+import { fromPromise } from 'rxjs/internal-compatibility';
 
 @Component({
     selector: 'course-dialog',
@@ -16,16 +16,16 @@ import {fromPromise} from 'rxjs/internal-compatibility';
 export class CourseDialogComponent implements OnInit, AfterViewInit {
 
     form: FormGroup;
-    course:Course;
+    course: Course;
 
-    @ViewChild('saveButton', { static: true }) saveButton: ElementRef;
+    @ViewChild('saveButton', { static: true, read: ElementRef }) saveButton: ElementRef;
 
-    @ViewChild('searchInput', { static: true }) searchInput : ElementRef;
+    @ViewChild('searchInput', { static: true }) searchInput: ElementRef;
 
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course ) {
+        @Inject(MAT_DIALOG_DATA) course: Course) {
 
         this.course = course;
 
@@ -33,22 +33,39 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
             description: [course.description, Validators.required],
             category: [course.category, Validators.required],
             releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
+            longDescription: [course.longDescription, Validators.required]
         });
 
     }
 
     ngOnInit() {
+        this.form.valueChanges
+            .pipe(
+                filter(() => this.form.valid),
+                mergeMap((values) => {
+                    return this.saveChanges(values);
+                })
+            ).subscribe()
+    }
 
-
-
+    saveChanges(values: any) {
+        return fromPromise(fetch('http://localhost:9000/api/courses/' + this.course.id, {
+            method: 'PUT',
+            body: JSON.stringify(values),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }))
     }
 
 
 
     ngAfterViewInit() {
-
-
+        fromEvent(this.saveButton.nativeElement, 'click')
+            .pipe(
+                tap(() => console.log('save button clicked')),
+                exhaustMap(() => this.saveChanges(this.form.value)),
+            ).subscribe()
     }
 
 
@@ -57,7 +74,7 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
         this.dialogRef.close();
     }
 
-  save() {
+    save() {
 
-  }
+    }
 }
